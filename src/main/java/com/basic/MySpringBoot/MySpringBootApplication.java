1732f529711ee56c04620ddb47692ab7fc5782d6
seoyeon020 <ksheee7000@naver.com>
2026-03-06 13:47:16 +0900

    first commit

---
+++ src/main/java/com/basic/MySpringBoot/MySpringBootApplication.java
@@ -0,0 +1,19 @@
+package com.basic.MySpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootApplication.class, args);
+	}
+
+    @Bean
+    public String Hello() {
+        System.out.println("====Spring Bean 입니다. ====");
+        return "Hello Bean";
+    }
+}

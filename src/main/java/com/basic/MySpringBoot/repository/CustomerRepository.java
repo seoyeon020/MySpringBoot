73bdd0a5c79f2331150913f5cd29c9df6403ce40
seoyeon020 <ksheee7000@naver.com>
2026-03-09 17:03:39 +0900

    update

--- src/main/java/com/basic/MySpringBoot/repository/CustomerRepository.java
+++ src/main/java/com/basic/MySpringBoot/repository/CustomerRepository.java
@@ -3,6 +3,15 @@ package com.basic.MySpringBoot.repository;
 import com.basic.MySpringBoot.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
+    //finder(query) method
+    //1. CustomerId(고객번호, Unique)로 조회하는 finder 메서드
+    Optional<Customer> findByCustomerId(String customerId);
+
+    //2. customerName(고객명, 중복허용)으로 조회하는 finder 메서드
+    List<Customer> findByCustomerNameContaining(String customerName);
 
 }

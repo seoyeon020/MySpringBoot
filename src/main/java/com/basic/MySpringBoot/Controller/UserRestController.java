d6f64d10066506992a041f29cabcb1c0897229de
seoyeon020 <ksheee7000@naver.com>
2026-03-11 02:08:50 +0900

    controller update

--- src/main/java/com/basic/MySpringBoot/Controller/UserRestController.java
+++ src/main/java/com/basic/MySpringBoot/Controller/UserRestController.java
@@ -1,7 +1,35 @@
 package com.basic.MySpringBoot.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.basic.MySpringBoot.entity.User;
+import com.basic.MySpringBoot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class UserRestController {
+    private final UserRepository userRepository;
+
+    //Constructor Injection
+//    public UserRestController(UserRepository userRepository) {
+//        log.info("UserRepository 구현클래스명 = {}",userRepository.getClass().getName());
+//        this.userRepository = userRepository;
+//    }
+
+    //User 등록
+    @PostMapping
+    public User create(@RequestBody User userDetail) {
+        return userRepository.save(userDetail);
+    }
+
+    //user 목록조회
+    @GetMapping
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
 }

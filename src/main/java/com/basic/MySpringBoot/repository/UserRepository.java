a3591572acd4590886f221a809a154d7de789a2b
seoyeon020 <ksheee7000@naver.com>
2026-03-10 16:08:00 +0900

    user table add

---
+++ src/main/java/com/basic/MySpringBoot/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.basic.MySpringBoot.repository;
+
+import com.basic.MySpringBoot.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    List<User> findByNameContains(String name);
+    Optional<User> findByEmail(String email);
+
+}

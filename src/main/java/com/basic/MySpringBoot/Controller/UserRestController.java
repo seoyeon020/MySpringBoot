b920f28311f8161dcd16a78f047742d7e245afac
seoyeon020 <ksheee7000@naver.com>
2026-03-11 11:26:06 +0900

    user controller update

--- src/main/java/com/basic/MySpringBoot/Controller/UserRestController.java
+++ src/main/java/com/basic/MySpringBoot/Controller/UserRestController.java
@@ -6,6 +6,7 @@ import com.basic.MySpringBoot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -41,5 +42,24 @@ public class UserRestController {
         User existUser = userRepository.findById(id)
                 .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));
         return existUser;
+    }
+
+    //Email 조회하고 수정하기
+    @PatchMapping("/{email}/")
+    public User updateUser(@PathVariable String email, @RequestBody User userDetail) {
+        User existUser = userRepository.findByEmail(email)
+                .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));
+        existUser.setName(userDetail.getName());
+        return userRepository.save(existUser);
+    }
+
+    //User 삭제하기
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));
+        userRepository.delete(user);
+        //return ResponseEntity.ok(user);
+        return ResponseEntity.ok().build();
     }
 }
